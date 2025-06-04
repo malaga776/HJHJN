cdedbd87f4ca8e2d771df4aa2809b92215e9b96c
malaga776 <malagama459@gmail.com>
2025-06-04 13:17:34 +0100

    Create React entry point

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-primary-50">
+      <h1 className="text-3xl font-heading text-primary-500">SafeFood</h1>
+    </div>
+  );
+}
+
+export default App;
